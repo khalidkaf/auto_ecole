69d786ca2b1ede1faa31b69252be3065c133b512
Dev10 <dev5@socializus.org>
2025-01-14 14:22:22 +0100

    test width

--- components/Header.js
+++ components/Header.js
@@ -9,31 +9,6 @@ import Navi from './Navi';
 const Header = () => {
   return (
     <header className={styles.header}>
-      {/* <div className={styles.logo}> */}
-        {/* <Link href="/k"> */}
-          {/* <a>MonSite</a> Logo ou titre */}
-          {/* <Image src={logo} alt={"logo entreprise"} width={120} height={60} /> */}
-        {/* </Link> */}
-      {/* </div> */}
-      {/* <nav>
-        <ul className={styles.navLinks}>
-          <li>
-            <Link href="/">
-              <a>ACCUEIL</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>CONTACT</a>
-            </Link>
-          </li>
-          <li className={styles.devis}>
-            <Link href="/contact">
-              <a>DEVIS GRATUIT</a>
-            </Link>
-          </li>
-        </ul>
-      </nav> */}
       <Navi/>
     </header>
   );
